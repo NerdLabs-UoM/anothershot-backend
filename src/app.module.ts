9a80ff33877208bc8b7d3b00ad508c52c8161ac6
Dheerasekara R J <125817506+ris-jeewa@users.noreply.github.com>
2024-01-03 23:42:50 +0530

    feat(ANSH-47): user management admin backend

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { PrismaService } from './prisma/prisma.service';
+import { AdminModule } from './admin/admin.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), UserModule, AuthModule],
+  imports: [ConfigModule.forRoot(), UserModule, AuthModule, AdminModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
