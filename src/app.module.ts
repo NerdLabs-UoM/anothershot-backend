721fa5a47362adcb379d42e43992d0182078fdee
PawaniGimhani <125270084+PawaniGimhani@users.noreply.github.com>
2024-02-26 12:06:00 +0530

    feat: implement endpoints for managing testimonials

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,10 +6,12 @@ import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { PrismaService } from './prisma/prisma.service';
 import { AdminModule } from './admin/admin.module';
-
+import { PhotographerController } from './photographer/photographer.controller';
+import { PhotographerService } from './photographer/photographer.service';
+import { PhotographerModule } from './photographer/photographer.module';
 @Module({
-  imports: [ConfigModule.forRoot(), UserModule, AuthModule, AdminModule],
-  controllers: [AppController],
-  providers: [AppService, PrismaService],
+  imports: [ConfigModule.forRoot(), UserModule, AuthModule, AdminModule, PhotographerModule],
+  controllers: [AppController, PhotographerController],
+  providers: [AppService, PrismaService, PhotographerService],
 })
 export class AppModule {}
---
+++ src/photographer/photographer.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { PhotographerController } from './photographer.controller';
+import { PhotographerService } from './photographer.service';
+import { PrismaService } from 'src/prisma/prisma.service';
+//import { JwtService } from '@nestjs/jwt';
+
+@Module({
+  imports: [],
+  controllers: [PhotographerController],
+  providers: [PhotographerService, PrismaService,],
+})
+export class PhotographerModule {}
