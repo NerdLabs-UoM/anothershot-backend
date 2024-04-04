c760810c65d15d80057235a2a6adda7a625d9d5a
Kavindu Wijegunasekara <110486479+Kavinduprabasara@users.noreply.github.com>
2024-04-04 14:29:18 +0530

    feat(ANSH-119): payment gateway and offers

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,9 +11,12 @@ import { ClientModule } from './client/client.module';
 import { ChatModule } from './chat/chat.module';
 import { ChatGateway } from './chat/chat.gateway';
 import { HomeModule } from './home/home.module';
+import {OfferModule} from './offer/offer.module';
+import {PaymentModule} from './payment/payment.module';
+import {StripeModule} from './stripe/stripe.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), UserModule, AuthModule, AdminModule, PhotographerModule, ClientModule, ChatModule, HomeModule],
+  imports: [ConfigModule.forRoot(), UserModule, AuthModule, AdminModule, PhotographerModule, ClientModule, ChatModule, HomeModule ,OfferModule ,PaymentModule, StripeModule],
   controllers: [AppController],
   providers: [AppService, PrismaService, ChatGateway],
 })
