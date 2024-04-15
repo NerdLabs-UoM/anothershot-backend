26dbbf6b097b5a2fa7539f5971f1d2221d4eec9e
PawaniGimhani <125270084+PawaniGimhani@users.noreply.github.com>
2024-04-15 13:25:34 +0530

    feat(ANSH-133): client profile backend

---
+++ src/client/dto/client.dto.ts
@@ -0,0 +1,20 @@
+import { IsString } from 'class-validator';
+
+export class ClientDto {
+    @IsString()
+    name: string;
+
+    @IsString()
+    bio: string;
+
+    @IsString()
+    clientId: string;
+}
+
+export class ClientImageDto {
+    @IsString()
+    image: string;
+
+    @IsString()
+    clientId: string;
+}
