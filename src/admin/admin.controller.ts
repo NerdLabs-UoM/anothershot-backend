fb38484459639ba68c3a60320ee2abad45192ba0
Dheerasekara R J <125817506+ris-jeewa@users.noreply.github.com>
2024-04-24 14:04:08 +0530

    feat(ANSH-164): fix search bar of user management table

--- src/admin/admin.controller.ts
+++ src/admin/admin.controller.ts
@@ -6,18 +6,18 @@ export class AdminController {
     constructor(private adminService: AdminService) { }
 
     @Get('getallusers')
-    async getAllUsers(@Query('page') page: number) {
+    async getAllUsers(@Query('page') page: number ,@Query('name') name:string) {
         try{
-            return await this.adminService.findall(page);
+            return await this.adminService.findall(page,name);
         }catch(err){
             console.log(err);
         }
     }
 
     @Get('getlastpage')
-    async getLastPage() {
+    async getLastPage(@Query('name') name: string) {
         try{
-            return await this.adminService.findLastPage();
+            return await this.adminService.findLastPage(name);
         }catch(err){
             console.log(err);
         }
