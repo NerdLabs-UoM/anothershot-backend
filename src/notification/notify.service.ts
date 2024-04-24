1b16d4972c7a59750c85e3b2fb9343e963a9257d
Dheerasekara R J <125817506+ris-jeewa@users.noreply.github.com>
2024-04-24 07:19:53 +0530

    feat(ANSH-7): add saved notifications

--- src/notification/notify.service.ts
+++ src/notification/notify.service.ts
@@ -1,61 +1,59 @@
 import { PrismaService } from 'src/prisma/prisma.service';
 import {
-  Injectable,
+    Injectable,
 } from '@nestjs/common';
 import { Photographer, User } from '@prisma/client';
 import { CreateNotifyDto, UpdateNotifyDto } from './dto/create-notify.dto';
- 
+
 
 @Injectable()
 export class NotifyService {
-    constructor(private prisma:PrismaService){}
+    constructor(private prisma: PrismaService) { }
 
-    async getNotifications(id:string){
+    async getNotifications(id: string) {
         return await this.prisma.notification.findMany({
-            where:{
-                receiverId:id
+            where: {
+                receiverId: id
             },
-            select:{
-                id:true,
-                type:true,
-                title:true,
-                description:true,
-                read:true,
-                createdAt:true,
-                receiver:{
-                    select:{
-                        id:true,
+            select: {
+                id: true,
+                type: true,
+                title: true,
+                description: true,
+                read: true,
+                createdAt: true,
+                receiver: {
+                    select: {
+                        id: true,
                     }
                 }
             }
         })
     }
 
-        async createNotification(dto:CreateNotifyDto){
-            const notification = await this.prisma.notification.create({
-                data: {
-                    receiver: {
-                      connect: {
+    async createNotification(dto: CreateNotifyDto) {
+        const notification = await this.prisma.notification.create({
+            data: {
+                receiver: {
+                    connect: {
                         id: dto.receiverId,
-                      },
                     },
-                    type: dto.type,
-                    title: dto.title,
-                    description: dto.description?dto.description:"",
-                }
-            })
-            console.log("notification==>",notification);
-            console.log("notification created by liked feed==>",dto);
-        }
-
-        async updateNotify(dto:UpdateNotifyDto){
-            return await this.prisma.notification.update({
-                where:{
-                    id:dto.id
                 },
-                data:{
-                    read:dto.read
-                }
-            })
-        }
+                type: dto.type,
+                title: dto.title,
+                description: dto.description ? dto.description : "",
+            }
+        })
     }
+
+    async updateNotify(dto: UpdateNotifyDto) {
+        return await this.prisma.notification.update({
+            where: {
+                id: dto.id
+            },
+            data: {
+                read: dto.read
+            }
+        })
+    }
+}
