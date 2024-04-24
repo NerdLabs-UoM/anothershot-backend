35ac01a58fe9ccc8fcf60d293579eede0722e18b
Dheerasekara R J <125817506+ris-jeewa@users.noreply.github.com>
2024-04-24 14:04:08 +0530

    feat(ANSH-164): fix search bar of user management table

--- src/admin/admin.service.ts
+++ src/admin/admin.service.ts
@@ -5,13 +5,20 @@ import { Injectable } from "@nestjs/common";
 export class AdminService {
   constructor(private prisma: PrismaService) { }
   
-  async findall(page:number){
+  async findall(page:number,name:string){
     const pageSize = 3;
     const skip = (page - 1) * pageSize;
     const take = pageSize;
+
+    let whereClause = {}; // Define an empty object for the where clause
+
+    if(name){
+      whereClause = { userName: { contains: name } }; // If name is provided, filter by username
+    }
     const values = await this.prisma.user.findMany({
       skip,
       take,
+      where: whereClause,
       include:{
       reports:true
     }
@@ -19,9 +26,17 @@ export class AdminService {
     return values;
   } 
 
-  async findLastPage(){
+  async findLastPage(name:string){
     const pageSize = 3;
-    const total = await this.prisma.user.count();
+    let whereClause = {};
+
+    if(name){
+      whereClause = { userName: { contains: name } };
+    }
+
+    const total = await this.prisma.user.count({
+      where: whereClause
+    });
     const lastPage = Math.ceil(total / pageSize);
     return lastPage;
   }
