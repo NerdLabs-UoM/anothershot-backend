4f9a316b4c09a4e97937b64329eab78a91eba558
Dheerasekara R J <125817506+ris-jeewa@users.noreply.github.com>
2024-05-19 17:54:15 +0530

    feat(ANSh-185): add web socket to notification system

--- src/photographer/photographer.module.ts
+++ src/photographer/photographer.module.ts
@@ -4,10 +4,11 @@ import { PrismaService } from "src/prisma/prisma.service";
 import { PhotographerController } from './photographer.controller';
 import { JwtService } from "@nestjs/jwt";
 import { NotifyService } from "src/notification/notify.service";
+import { AppGateway } from "src/app.gateway";
 
 @Module({
     controllers: [PhotographerController],
-    providers: [PhotographerService, PrismaService, JwtService,NotifyService],
+    providers: [PhotographerService, PrismaService, JwtService,NotifyService,AppGateway],
 })
 
 export class PhotographerModule { }
