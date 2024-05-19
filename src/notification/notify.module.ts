130f3c8ad840b46ffc74fa00eda85c98a67327f5
Dheerasekara R J <125817506+ris-jeewa@users.noreply.github.com>
2024-05-19 17:54:15 +0530

    feat(ANSh-185): add web socket to notification system

--- src/notification/notify.module.ts
+++ src/notification/notify.module.ts
@@ -2,11 +2,11 @@ import { Module } from '@nestjs/common';
 import { NotifyService } from './notify.service';
 import { NotifyController } from './notify.controller';
 import { PrismaService } from 'src/prisma/prisma.service';
-
+import { AppGateway } from 'src/app.gateway';
 
 @Module({
     controllers: [NotifyController],
-    providers: [NotifyService,PrismaService]
+    providers: [NotifyService,PrismaService,AppGateway]
 })
 
 export class NotifyModule{}
