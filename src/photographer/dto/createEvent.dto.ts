9473bf6350400772f6b4a84fc24ba7d4fb94267d
Amarasena B.H.L.M <141402575+LahiruniMalshika@users.noreply.github.com>
2024-05-25 13:20:19 +0530

    Feature/ANSH-128/photographer bookings add to Calendar  (#46)
    * feat(ANSH-128): update bookings
    
    * feat(ANSH-128): update photographerBookings
    
    * feat(ANSH-128): update bookings
    
    * feat(ANSH-128): update bookings
    
    * feat(ANSH-128): update bookings
    
    * feat(ANSH-128): update bookings
    
    * feat(ANSH-128): update bookings
    
    * feat(ANSH-128): update bookings
    
    * feat(ANSH-128): update bookings
    
    * feat(ANSH-128): update bookings
    
    * feat(ANSH-128): update bookings
    
    * feat(ANSH-128): update bookings
    
    * feat(ANSH-128): update bookings

---
+++ src/photographer/dto/createEvent.dto.ts
@@ -0,0 +1,25 @@
+import {  IsBoolean, IsDate,  IsDateString,  IsOptional, IsString } from "class-validator";
+
+export class createEventDto{
+       
+    @IsString()
+    title: string;
+
+    @IsString()
+    bookingId: string;
+
+    @IsString()
+    description:string;
+
+    @IsDateString()
+    start: string;
+
+    @IsDateString()
+    end: string;
+        
+    @IsBoolean()
+    @IsOptional()
+    allDay: boolean;
+
+
+}
