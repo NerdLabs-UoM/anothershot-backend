2d0ae814b9b154d2a5b2fe7ab14c6c7ecaa27b78
Dheerasekara R J <125817506+ris-jeewa@users.noreply.github.com>
2024-06-12 09:50:59 +0530

    fix(ANSH): fix photographer module

--- src/photographer/photographer.module.ts
+++ src/photographer/photographer.module.ts
@@ -6,11 +6,12 @@ import { JwtService } from "@nestjs/jwt";
 import { NotifyService } from "src/notification/notify.service";
 import { AppGateway } from "src/app.gateway";
 import { AlbumsModule } from './albums/albums.module';
+import { SettingsModule } from "./settings/settings.module";
 
 @Module({
     controllers: [PhotographerController],
     providers: [PhotographerService, PrismaService, JwtService,NotifyService,AppGateway],
-    imports: [AlbumsModule],
+    imports: [AlbumsModule,SettingsModule],
 })
 
 export class PhotographerModule { }
