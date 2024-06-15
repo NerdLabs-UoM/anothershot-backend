41864326bf1b9ce92b0b4ba8caa9684a0b907217
Amarasena B.H.L.M. <lahimalshi@gmail.com>
2024-06-16 00:38:25 +0530

    feat(ANSH-222): update features

--- src/dashboard/dashboard.controller.ts
+++ src/dashboard/dashboard.controller.ts
@@ -2,15 +2,34 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { DashboardService } from './dashboard.service';
 
 
-@Controller('dashboard')
+@Controller('api/dashboard')
 export class DashboardController {
   constructor(private readonly dashboardService: DashboardService) {}
 
-  @Get('totalBooinkgs')
+  @Get('totalRevenue')
+  async getTotalRevenue() {
+    return this.dashboardService.getTotalRevenue();
+  }
+
+  @Get('totalUsers')
+  async getTotalUsers() {
+    return this.dashboardService.getTotalUsers();
+  }
+
+  @Get('totalBookings')
   async getTotalBookings() {
     return this.dashboardService.getTotalBookings();
   }
 
+  @Get('activeUsers')
+  async getActiveUsers() {
+    return this.dashboardService.getActiveUsers();
+  }
+
+  @Get('recentPayments')
+  async getRecentPayments() {
+    return this.dashboardService.getRecentPayments();
+  }
 
   
 }
--- src/dashboard/dashboard.service.ts
+++ src/dashboard/dashboard.service.ts
@@ -1,7 +1,6 @@
 import { PrismaService } from 'src/prisma/prisma.service';
 import { HttpException, HttpStatus, Injectable, Logger } from "@nestjs/common";
 
-
 @Injectable()
 
 export class DashboardService {
@@ -9,8 +8,50 @@ export class DashboardService {
   private readonly logger = new Logger(DashboardService.name);
   
   constructor(private prisma: PrismaService) { }
+  
+
+async getTotalRevenue() {
+  const totalRevenue = await this.prisma.payment.aggregate({
+    _sum: {
+      amount: true,
+    },
+  });
+  return totalRevenue._sum.amount || 0;
+}
+
+  async getTotalUsers() {
+    const totalUsers = await this.prisma.user.count({
+    });
+    return totalUsers;
+  }
+
   async getTotalBookings() {
-    const total = await this.prisma.booking.count();
-    return total;
+    const totalBookings = await this.prisma.booking.count();
+    return totalBookings;
+  }
+
+  async getActiveUsers() {
+    const activeUsers = await this.prisma.user.count({
+      where: {
+        isActive: true,
+      },
+    });
+  }
+
+  async getRecentPayments() {
+    const recentPayments = await this.prisma.payment.findMany({
+      take: 5,
+      orderBy: {
+        createdAt: 'desc',
+      },
+      include: {
+        client: {
+          select: {
+            name: true,
+        },
+      },
+    },
+  });
+    return recentPayments;
   }
 }
